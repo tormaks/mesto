4d2cb7dc05b0d7ad91e395b4ea249af4a0cf8a39
Maksim Torchinskiy <roadtopatience@gmail.com>
2021-11-15 13:27:20 +0300

    refactor: изменить кнопки и popup

--- scripts/script.js
+++ scripts/script.js
@@ -13,10 +13,6 @@ let profileJob = document.querySelector('.profile__job');
 for (let i = 0; i < buttonLike.length; i++) { 
 	buttonLike[i].addEventListener('click', function () {
 		buttonLike[i].classList.toggle('card__like_active');
-		if (buttonLike[i].classList.contains('card__like_active')) {
-			buttonLike[i].innerHTML = '<img src="./images/likeblack.svg" alt="">';
-
-		} else buttonLike[i].innerHTML = '<img src="./images/like.svg" alt="">';
 	});
 }
 
