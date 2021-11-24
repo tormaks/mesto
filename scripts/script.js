bee993451a5a95673c3174c8415ef7920cae0665
Maksim Torchinskiy <roadtopatience@gmail.com>
2021-11-24 13:31:43 +0300

    feat: загрузка карточек с помощью js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,53 @@
 'use strict'
 
-let buttonEditProfile = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let buttonClosePopup = popup.querySelector('.popup__close');
-let popupForm = popup.querySelector('form');
-let inputName = popupForm.querySelector('.popup__input_value_name');
-let inputJob = popupForm.querySelector('.popup__input_value_job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let buttonsLike = document.querySelectorAll('.card__like');
+const initialCards = [
+	{
+		name: 'Петергоф',
+		link: '../images/peterhof.jpg',
+	},
+	{
+		name: 'Невский Проспект',
+		link: '../images/nevskijprospekt.jpg',
+	},
+	{
+		name: 'Камчатка',
+		link: '../images/kamchatka.jpg',
+	},
+	{
+		name: 'Байкал, Иркутск',
+		link: '../images/baikal.jpg',
+	},
+	{
+		name: 'Алтай',
+		link: '../images/altai.jpg',
+	},
+	{
+		name: 'Карелия',
+		link: '../images/kareliya.jpg',
+	},
+]
+
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const buttonClosePopup = popup.querySelector('.popup__close');
+const popupForm = popup.querySelector('form');
+const inputName = popupForm.querySelector('.popup__input_value_name');
+const inputJob = popupForm.querySelector('.popup__input_value_job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const buttonsLike = document.querySelectorAll('.card__like');
+const cardTemplate = document.querySelector('#card').content;
+const cardsList = document.querySelector('.cards__list');
+
+
+//Добавление начальных карточек
+function firstCards(card) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	cardElement.querySelector('.card__image').src = card.link;
+	cardElement.querySelector('.card__image').alt = card.name;
+	cardElement.querySelector('.card__heading').textContent = card.name; 
+	cardsList.append(cardElement);
+}
 
 //Поставить лайк карточке
 function putLike(buttonLike) {
@@ -37,10 +76,12 @@ function sendForm(evt) {
 	closePopup();
 }
 
+initialCards.forEach(firstCards);
 buttonEditProfile.addEventListener('click', openPopup);
 buttonClosePopup.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', sendForm);
 buttonsLike.forEach(putLike);
+
 
 
 
